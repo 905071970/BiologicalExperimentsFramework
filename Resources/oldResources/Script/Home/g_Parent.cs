5bcdb683537d1a81507208c45253fd93e1dae74c
MLZZF <905071970@qq.com>
2021-03-03 21:16:27 +0800

    delete all

--- Resources/oldResources/Script/Home/g_Parent.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using UnityEngine;
-
-public class g_Parent : MonoBehaviour
-{
-    //绑定父物体，back按钮寻找并激活父物体，关闭该对象
-    public GameObject g_parent;
-}
--- Scripts/ScriptObject/CameraPosRotObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CameraPosRotObject : ScriptableObject
-{
-    public Vector3 pos;
-    public Quaternion rot;
-}
